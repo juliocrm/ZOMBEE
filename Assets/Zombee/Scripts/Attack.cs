42192d69342422c2553512858f354929d16f03b1
Julio <julcrm@gmail.com>
2019-01-26 14:38:34 -0500

    Add Attack behaviour
    Waiting for GameManager -> weaponsDef

--- Assets/Zombee/Scripts/Attack.cs
+++ Assets/Zombee/Scripts/Attack.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : Entity
+public class AttackComp : Entity
 {
+    private WeaponDef _currentWeapon;
+    private bool attacking = true;
+
     public override void Die()
     {
-        throw new System.NotImplementedException();
+        WeaponEmpty();
     }
 
-    public Attack()
+    public AttackComp()
     {
+        
+    }
 
+    public void Attack()
+    {
+        if(_currentWeapon.durability > 0)
+            attacking = true;
+    }
+
+    public void SetWeapon(int weaponID)
+    {
+        //Get weapon from GameManager
+        //_currentWeapon = GameManager.Instance.weaponDefs[weaponID];
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (attacking == false) return;
+
+        ContactPoint[] contacts = null;
+        collision.GetContacts(contacts);
+
+        foreach (var contact in contacts)
+        {
+            if (contact.otherCollider.GetComponent<IHurtable>() != null)
+            {
+                contact.otherCollider.GetComponent<IHurtable>().Hurt(_currentWeapon.damage);
+
+                _currentWeapon.durability--;
+                if (_currentWeapon.durability <= 0)
+                {
+                    WeaponEmpty();
+                    break;
+                }
+            }
+        }
+        
+        attacking = false;
+    }
+
+    private void WeaponEmpty() {
+        // TODO Destroy weapon
     }
 }
