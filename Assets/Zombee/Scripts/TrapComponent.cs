758cd3a18b57d98638d8c5186ce86d91c26fa7df
Esteban Ramos <esteban.ramos@fractal-vr.com>
2019-01-26 20:28:38 -0500

    Trap Component Damage and weak

--- Assets/Zombee/Scripts/TrapComponent.cs
+++ Assets/Zombee/Scripts/TrapComponent.cs
@@ -6,46 +6,78 @@ public class TrapComponent : MonoBehaviour
 {
     public TrapDef trapDefinition;
     public GameObject TrapPrefab;
+
     private bool TrapIsActive = false;
+    private bool TrapIsEnable = false;
+
+    [SerializeField]
+    public Transform _overlapSphereTransform;
+    [SerializeField]
+    public float _sphereRadious;
+
+    public Vector3 OverlapSpherePosition { get { return _overlapSphereTransform.position; } }
+
 
     public void InstantiateTrap(Vector3 Position) {
         //Create a prefab copy in the point 
         //Instantiate in position
         Instantiate(TrapPrefab, Position, new Quaternion()); 
-        TimeToExecuteTrap(trapDefinition.TimeToActive);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("toca");
+        if (!TrapIsEnable && collision.gameObject.GetComponent<EnemyAI>())
+        {
+            Debug.Log("activa");
+            TrapIsEnable = true;
+            StartCoroutine(TimeToExecuteTrap(trapDefinition.TimeToActive));
+        }
     }
     private IEnumerator TimeToExecuteTrap(float Time) {
         yield return new WaitForSeconds(Time);
-        ExecuteTrap();
-    }
-    private void ExecuteTrap() {
         TrapIsActive = true;
-        StartCoroutine(DeactiveTrap());
-    }
-    private IEnumerator DeactiveTrap() {
         yield return new WaitForSeconds(trapDefinition.TimeActive);
         TrapIsActive = false;
         Destroy(TrapPrefab);
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        EnemyHP enemyHP = collision.gameObject.GetComponent<EnemyHP>();
-        if (enemyHP != null) {
-            switch (trapDefinition.TrapType) {
-                case TrapType.Damage:
-                    //Damage to enemy
-                    break;
-                case TrapType.Slow:
-                    //enemi is slow
-                    break;
-                case TrapType.Turn:
-                    //fight enemies
-                    break;
-                case TrapType.Weak:
-                    //...
-                    break;
 
+    private List<EnemyHP> EnemiesAfected=new List<EnemyHP>();
+    private void Update()
+    {
+        if (TrapIsActive)
+        {
+            Physics.OverlapSphere(_overlapSphereTransform.position,
+                _sphereRadious);
+            Collider[] _collider = Physics.OverlapSphere(transform.position, 2f);
+            foreach (var contact in _collider)
+            {
+                switch (trapDefinition.TrapType) {
+                    case TrapType.Damage:
+                        EnemyHP Enemi = contact.GetComponent<EnemyHP>();
+                        if (Enemi = null) {
+                            if (!EnemiesAfected.Contains(Enemi)) {
+                                Enemi.Hurt(trapDefinition.Damage);
+                            }
+                        }
+                        break;
+                    case TrapType.Slow:
+                        //Ejecutar lentitud
+                        break;
+                    case TrapType.Turn:
+                        //Ejecutar turn
+                        break;
+                    case TrapType.Weak:
+                        EnemyHP Enemi2 = contact.GetComponent<EnemyHP>();
+                        if (Enemi2 = null)
+                        {
+                            Enemi2.SetDamageMultiplier(trapDefinition.Damage, trapDefinition.TimeActive);
+                        }
+                        break;
+                }
             }
         }
     }
+
+
+
 }
