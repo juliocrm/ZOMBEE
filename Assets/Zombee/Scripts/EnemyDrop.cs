72bf98cbab5b6fe59de5b885d5c747d818c1544a
Julio <julcrm@gmail.com>
2019-01-26 20:18:25 -0500

    Add EnemyDrop behaviour

--- Assets/Zombee/Scripts/EnemyDrop.cs
+++ Assets/Zombee/Scripts/EnemyDrop.cs
@@ -1,23 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyDrop : Entity
 {
+    [SerializeField]
+    private GameObject _dropWeaponPrefab;
+
+    [SerializeField]
+    private float probability = 0.5f;
+
+    private const float spawnHeight = 1;
+
     public override void Die()
     {
-        throw new System.NotImplementedException();
+        var value = Random.Range(0,101);
+        if(value <= probability * 100)
+        {
+            DropWeapon();
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void DropWeapon()
     {
-        
-    }
+        var spawnPos = transform.position;
+        spawnPos.y += spawnHeight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instantiate(_dropWeaponPrefab, spawnPos, Quaternion.identity);
     }
 }
