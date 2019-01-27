141c6247021ca77b48123927d00805e64aff66fd
aldiazj <alejandrodiazjllo@gmail.com>
2019-01-27 10:59:59 -0500

    Adds TurnAgainst to the EnemyIA

--- Assets/Zombee/Scripts/Entities/EnemyAI.cs
+++ Assets/Zombee/Scripts/Entities/EnemyAI.cs
@@ -16,6 +16,8 @@ public class EnemyAI : Entity
 
     [SerializeField]
     float enemyScope = 15;
+    [SerializeField]
+    public float checkSphereRadious = 5;
 
     private Transform playerTransform;
     private readonly int layerMask = 1 << 9;
@@ -32,6 +34,7 @@ public class EnemyAI : Entity
 
     bool isAlive = true;
     bool playerOnSight;
+    bool isTurned;
 
 
     private void Awake()
@@ -138,6 +141,30 @@ public class EnemyAI : Entity
         return false;
     }
 
+    private Transform CheckForEnemies()
+    {
+        Collider[] _collider = Physics.OverlapSphere(enemyTransform.position, checkSphereRadious);
+        float nearestEnemyDistance = Mathf.Infinity;
+        Collider nearestEnemy = null;
+        foreach (Collider contact in _collider)
+        {
+            if (contact.gameObject != gameObject && contact.GetComponent<IHurtable>() != null && !contact.GetComponent<PlayerController>())
+            {
+                float distanceToEnemy = (contact.transform.position - enemyTransform.position).magnitude;
+                if (distanceToEnemy < nearestEnemyDistance)
+                {
+                    nearestEnemy = contact;
+                    nearestEnemyDistance = distanceToEnemy;
+                }
+            }
+        }
+        if (nearestEnemy)
+        {
+            return nearestEnemy.transform;
+        }
+        return null;
+    }
+
     private int destPoint = 0;
 
     void ChangeTarget()
@@ -156,6 +183,36 @@ public class EnemyAI : Entity
             newDest = UnityEngine.Random.Range(0, PatrolPoints.Length);
 
         destPoint = newDest;
+    }
+
+    public void TurnAgainst()
+    {
+        isTurned = true;
+        StartCoroutine(TurnAgainstProcess());
+        StartCoroutine(FinishTurnProcess());
+    }    
+
+    IEnumerator TurnAgainstProcess()
+    {
+        Transform tempPlayer = playerTransform;
+        EnemyType previousEnemyType = enemyType;
+        enemyType = EnemyType.Assasin;
+        while (isTurned)
+        {
+            Transform newEnemy = CheckForEnemies();
+            playerTransform = newEnemy;
+            yield return waitUntilNextDecition; 
+        }
+        playerTransform = tempPlayer;
+        enemyType = previousEnemyType;
+    }
+
+    readonly WaitForSeconds waitForTurnBack = new WaitForSeconds(3);
+
+    IEnumerator FinishTurnProcess()
+    {
+        yield return waitForTurnBack;
+        isTurned = false;
     }
 
     public override void Die()
