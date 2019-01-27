9fe5e394edd8b8a9e5cc95faa5a56e44a6e5576d
Esteban Ramos <esteban.ramos@fractal-vr.com>
2019-01-26 19:56:51 -0500

    Remake pickuphealthScript

--- Assets/Zombee/Scripts/PickupHealth.cs
+++ Assets/Zombee/Scripts/PickupHealth.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class PickupHealth : MonoBehaviour
 {
-    public int Seconds;
     public int StaminaIncrease;
+    public int Time;
     Stamina staminaComponent;
+    bool Incontact = false;
     private void OnCollisionEnter(Collision collision)
     {
         staminaComponent= collision.gameObject.GetComponent<Stamina>();
-        StartCoroutine(IncreaseStamina());
-    }
-    private IEnumerator IncreaseStamina() {
-        yield return new WaitForSeconds(Seconds);
         if (staminaComponent != null) {
+            Incontact = true;
+            StartCoroutine(TimeToReceibeStamina());
+        }   
+    }
+    private IEnumerator TimeToReceibeStamina() {
+        yield return new WaitForSeconds(Time);
+        if (Incontact) {
             staminaComponent.Hurt(StaminaIncrease);
-            StartCoroutine(IncreaseStamina());
+            gameObject.SetActive(false);
         }
     }
     private void OnCollisionExit(Collision collision)
     {
-        if (staminaComponent != null && collision.gameObject.GetComponent<Stamina>()) {
-            StopCoroutine(IncreaseStamina());
+        if (staminaComponent == collision.gameObject.GetComponent<Stamina>())
+        {
+            StopCoroutine(TimeToReceibeStamina());
+            Incontact = false;
             staminaComponent = null;
         }
     }
-
 }
